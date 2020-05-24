333a346c17db43b6a116026fde12a17d908d1b32
YuriiChernii <65872689+YuriiChernii@users.noreply.github.com>
2020-05-25 00:52:41 +0300

    Add files via upload

---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tymchak_shop.Data.Models;
+
+namespace Tymchak_shop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Shoes> favouriteShoes { get; set; }
+    }
+}
---
+++ ViewModels/ShoesListViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tymchak_shop.Data.Models;
+
+namespace Tymchak_shop.ViewModels
+{
+    public class ShoesListViewModels
+    {
+        public IEnumerable<Shoes> allShoes { get; set; }
+        public string currCategory { get; set; }
+    }
+}
